96824938420c03929fd0563e03f1bbafef9faca4
aman-kaur11 <63080661+aman-kaur11@users.noreply.github.com>
2020-04-16 14:47:45 -0700

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -1,16 +1,15 @@
 <html>
-<html lang="en" dir="ltr">
   <head> <!This is the title of the webpage>
     <meta charset="utf-8">
     <title>Bank Name</title>
     <link rel="stylesheet" href="style.css">
   </head>
 
+
   <body>  <!This is the title page>
     <div id = "grad1", class = "header"><h1>
-			<p class = "custom1"> BANK NAME</p>
-		</h1></div>
-
+     <p class = "custom1"> BANK NAME</p>
+   </h1></div>
 		<div class="topnav">
           <!This is the sign up button, will add a connection later>
 			<a href="" style="float: right;"> Sign Out</a>
@@ -23,21 +22,21 @@
 				</div>
 			</div>
     </div>
-
     <h2></h2>
+
+
     <div class="topnav">
           <!This is the Transaction section: NEED TO INSERT IMAGE>
       <div id = "grad2", class = "header"><h1>
         <p class = "custom1"> Transactions</p>
       </h1>
-
         <a href="" style="float: center;"> Transfer</a>
         <a href="" style="float: center;"> Withdraw</a>
         <a href="" style="float: center;"> Deposit </a>
       </div>
     </div>
-
     <h2></h2>
+
     <div class="topnav">
           <!This is the Account History section: NEED TO INSERT IMAGE>
       <div id = "grad2", class = "header"><h1>
@@ -47,8 +46,8 @@
         <a href="" style="float: center;"> Savings Account</a>
       </div>
     </div>
-
     <h2></h2>
+
     <div class="topnav">
           <!This is the Account Settings section: NEED TO INSERT IMAGE>
       <div id = "grad2", class = "header"><h1>
@@ -56,5 +55,6 @@
         <button type="button" onclick="alert('Directing...')">Make Changes</button>
       </h1></div>
     </div>
+    
   </body>
 </html>
