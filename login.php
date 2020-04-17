b9c68bfcf27ba6ce86d684ab61c3747e15b804e6
laurenmacpherson <60455659+laurenmacpherson@users.noreply.github.com>
2020-04-16 17:50:54 -0700

    Add files via upload
    New userpage styling idea

--- login.php
+++ login.php
@@ -1,26 +1,50 @@
-<?php
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="registerStyle.css">
+	<!At this point in time, I will be using the registerStyle for the login page as well>
+  </head>
+  <body>
+
+  <div id= "form">
+    <form action = "login.php" method="post">
+	<! Login currently set to use username and password for login, may want to include email option>
+      <div class = "form-group">
+        <label for = "Username"> Username: </label>
+        <input name = "Username" id = "Username">
+      <div class = "form-group">
+        <label for ="Password">Password: </label>
+        <input name ="Password" id = "Password">
+      </div>
+        <input type = "submit" name = "submit" id = "submit">
+	</form>
+  </div>
+
+  <?php
     session_start();
-    
+
     $errorRet = "";
     if(isset($_POST["loginuser"])){
         $username = $_POST["loginuser"];
     } else {
         $username = "";
     }
-    
+
     if(isset($_POST["loginpass"])){
         $password = $_POST["loginpass"];
     } else {
         $password = "";
     }
-    
+
     $password = hash('password', $password);
 
     $conn = new mysqli("localhost", "bankname", "frankbutt!", "bankname");
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-	
+
 	$sql = "SELECT * FROM Users;";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
@@ -46,9 +70,9 @@
 	} else {
 	    $errorRet = "3";
 	}
-    
+
     header('Location: /login/?return='.$errorRet);
     exit;
-	
+
     $conn->close();
 ?>
