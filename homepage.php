1298a8669e054342dd76bfbc6ef6deaf32d8d29e
aman-kaur11 <63080661+aman-kaur11@users.noreply.github.com>
2020-04-20 02:06:51 -0700

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -6,109 +6,140 @@
     <link rel="stylesheet" href="homepage.css">
   </head>
 
-  <body>  <!This is the title page>
-
-<!MENU BAR>
-  <div id="mySidenav" class="sidenav">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="">Checking Account</a>
-    <a href="">Savings Account</a>
-    <a href="">Investing</a>
-    <a href="">Better Money Habits</a>
-  </div>
-
-  <div id="main">
-    <div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</div>
-
-<div style="padding-left:16px">
-
-</div>
-    <div class="header">
-    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; MENU</span>
-    <!TOP BAR>
-
-    <a href="homepage.php" class="logo">Bank Name</a>
-    <div class="header-right">
-      <a class="active" href="#home">Sign In</a>
+  <body><!This is the title page>
+    <!MENU BAR>
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="">Checking Account</a>
+      <a href="">Savivngs Account</a>
+      <a href="">Investing</a>
+      <a href="">Better Money Habbits</a>
     </div>
 
+    <div id="main">
+      <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
 
-  </div>
+      <div class="header">
+        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; MENU</span>
+        <!TOP BAR>
+        <a href="homepage.php", class="logo", style="color: white">Bank Name</a>
+        <div class="header-right">
+          <a class="active" href="#home">Sign In</a>
+        </div>
+      </div>
 
-  <div style="padding-left:20px">
-    <!SIGN IN>
-    <section><h1>TEST SECTION</h1></section>
-
-    <!REGISTER>
-        <h2></h2>
+      <!Register>
+      <div class = "section">
         <div class = "row">
-          <div class = "leftcolumn">
-            <div class= "account">
-              <h1><p> Register Today!</p></h1>
-                <div class="topnav2">
-                  <a href="register.php" style="float: left;">Register</a>
-                </div>
-            </div>
+          <div class="leftcolumn">
+            <img src="space.jpg" alt="Image 2">
           </div>
 
-          <div class = "rightcolumn">
-            <div class = "account">
-              <h1> Sign In</h1>
-              <div class="topnav2">
-                <a href="register.php" style="float: left;"> Let's get going!</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!This is for bottom of homepage>
-        <h1></h1>
-        <div class="row:after">
-        <div class="column">
-        <div class="container">
-          <img src="cool.jpg" alt="Avatar" class="image">
-          <div class="overlay">
-            <div class="text">Take Control of your finances</div>
-          </div>
-        </div>
-        </div>
-
-        <div class="column">
-        <div class="container">
-          <img src="hover.jpg" alt="Avatar1" class="image">
-          <div class="overlay">
-            <div class="text">Take Control of your finances</div>
+          <div class="rightcolumn">
+            <h1>Put Some Catchy Logo Here</h1>
+            <p>Put some other stuff here and add a button</p>
+            <br>
+            <a href = "register.php", button class="button button1">Register</a>
           </div>
         </div>
       </div>
 
-      <div class="column">
-      <div class="container">
-        <img src="thing.jpeg" alt="Avatar2" class="image">
-        <div class="overlay">
-          <div class="text">Take Control of your finances</div>
+      <br>
+
+      <!something else>
+      <div class = "section">
+        <div class="row">
+          <div class="leftcolumn">
+            <h1>Put Some Catchy Logo Here</h1>
+            <p>Put some other stuff here and add a button</p>
+            <br>
+            <a href = "", button class="button button1">This can go to something</a>
+          </div>
+
+          <div class="leftcolumn">
+            <img src="money.jpg" alt="Image 2" style="border-radius:50%">
+          </div>
         </div>
       </div>
+
+      <!Testemonials>
+      <br>
+      <div class = "section">
+        <div class="slideshow-container">
+          <div class="mySlides">
+            <q>I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
+            <p class="author">- John Keats</p>
+          </div>
+
+          <div class="mySlides">
+            <q>But man is not made for defeat. A man can be destroyed but not defeated.</q>
+            <p class="author">- Ernest Hemingway</p>
+          </div>
+
+          <div class="mySlides">
+            <q>I have not failed. I've just found 10,000 ways that won't work.</q>
+            <p class="author">- Thomas A. Edison</p>
+          </div>
+
+          <a class="prev" onclick="plusSlides(-1)">❮</a>
+          <a class="next" onclick="plusSlides(1)">❯</a>
+
+        </div>
+
+        <div class="dot-container">
+          <span class="dot" onclick="currentSlide(1)"></span>
+          <span class="dot" onclick="currentSlide(2)"></span>
+          <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+
+        <script>
+          var slideIndex = 1;
+          showSlides(slideIndex);
+
+          function plusSlides(n) {
+            showSlides(slideIndex += n);
+          }
+
+          function currentSlide(n) {
+            showSlides(slideIndex = n);
+          }
+
+          function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+          }
+        </script>
       </div>
+
+      <br>
     </div>
-  </div>
-</div>
 
-  <script>
-    function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
-    }
-    function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft= "0";
-    }
-  </script>
+    <script>
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+      }
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+      }
+    </script>
 
   </body>
 </html>
