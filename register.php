037c62664e3c93e014b469b7799e0beee34f86b6
victorlu27 <victor.lu@sjsu.edu>
2020-04-22 17:36:44 -0700

    Merges register-account.php into register.php

--- register.php
+++ register.php
@@ -10,7 +10,7 @@
 
 
 <div id= "form">
-    <form action = "register-account.php" method="post">
+    <form action = "register.php" method="post">
         <div class = "form-group">
           <label for ="First name"> First name:</label>
           <input name = "fname" type = "text">
@@ -33,6 +33,52 @@
         <input type = "submit" name = "submit" id = "submit">
    </form>
 </div>
+<?php
+  if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["username"])
+      && isset($_POST["password"]) && isset($_POST["email"])){
+    if ($_POST["fname"] && $_POST["lname"] && $_POST["username"] && $_POST["password"]
+        && $_POST["email"]){
 
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+
+    //Create connection
+    $conn = mysqli_connect("localhost", "root", "", "userbank");
+
+    //Check connection
+    if(!$conn){
+        die("Connection failed" . mysqli_connect_error());
+    }
+
+    //Register user
+    $sql = "INSERT INTO bankaccount (fname, lname, username, password, email) VALUES
+            ('$fname','$lname','$username','$password','$email')";
+
+    // echo $sql;
+    $results = mysqli_query($conn, $sql);
+
+    if ($results) {
+
+        sleep(3);
+        header("location: homepage.php");
+        echo "Registered.";
+
+    } else {
+        echo mysqli_error($conn);
+      }
+
+      mysqli_close($conn); // close connection
+
+
+    } else {
+      echo "A field is empty.";
+    }
+  } //else {
+      //echo "Form was not submitted.";
+    // }
+    ?>
   </body>
 </html>
