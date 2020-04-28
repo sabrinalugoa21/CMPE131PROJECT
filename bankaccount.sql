7d16e2bf6695b45036cc7de72c26e29c1c0a3cf8
aman-kaur11 <63080661+aman-kaur11@users.noreply.github.com>
2020-04-28 09:59:10 -0700

    bank account database

--- bankaccount.sql
+++ bankaccount.sql
@@ -1,63 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 23, 2020 at 09:22 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `userbank`
---
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bankaccount`
+-- Table structure for table `accounts`
 --
 
-CREATE TABLE `bankaccount` (
-  `fname` varchar(255) NOT NULL,
-  `lname` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `balance` float(10,2) NOT NULL,
-  `acctnum` int(11) NOT NULL
+CREATE TABLE `accounts` (
+  `userID` int(11) NOT NULL,
+  `acctNum` int(11) NOT NULL,
+  `acctName` varchar(255) DEFAULT NULL,
+  `balance` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `bankaccount`
+-- Indexes for table `accounts`
 --
-
-INSERT INTO `bankaccount` (`fname`, `lname`, `username`, `password`, `email`, `balance`, `acctnum`) VALUES
-('Lily', 'Ann', 'lilyann', 1234, 'lann@gmail.com', 0.00, 107451);
-
---
--- Indexes for dumped tables
---
+ALTER TABLE `accounts`
+  ADD UNIQUE KEY `acctNum` (`acctNum`);
 
 --
--- Indexes for table `bankaccount`
+-- AUTO_INCREMENT for dumped tables
 --
-ALTER TABLE `bankaccount`
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `acctnum` (`acctnum`);
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `acctNum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=195096;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
