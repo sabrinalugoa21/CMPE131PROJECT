ae8c43d9dd06b36af1d37ee5eb57a1b2eeab51a3
aman-kaur11 <63080661+aman-kaur11@users.noreply.github.com>
2020-04-28 10:02:40 -0700

    delete.php is needed within deleteAccount.php

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+//connnect to localhost
+$conn = mysqli_connect("localhost","root","","bankaccount");
+echo "connected! <br>";
+//check connection
+if(!$conn){
+    die("Connection failed" . mysqli_connect_error());
+}
+//select $query
+$sql = "DELETE FROM accounts WHERE userID = '$_GET[id]'";
+
+//execute $query
+if(mysqli_query($conn,$sql))
+  header("refresh:1; url = deleteAccount.php");
+else {
+    echo "NOT DELETED";
+}
+?>
