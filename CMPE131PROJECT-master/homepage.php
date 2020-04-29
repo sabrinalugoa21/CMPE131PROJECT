ea27e48d32a0467639b1c8667165ad54f940a737
laurenmacpherson <60455659+laurenmacpherson@users.noreply.github.com>
2020-04-29 13:44:21 -0700

    Add files via upload
    Added links, making some links work (when you click, it goes to the correct page). Not all links work. Added a checking file, which will be used to display info from the checking account. Need to initialize a checking and savings account during registration. *Deleted some files*

--- CMPE131PROJECT-master/homepage.php
+++ CMPE131PROJECT-master/homepage.php
@@ -1,151 +1,397 @@
-<html lang="en">
-  <head> <!This is the title of the webpage>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bank Name</title>
-    <link rel="stylesheet" href="homepage.css">
-  </head>
-
-  <body><!This is the title page>
-    <!MENU BAR>
-    <div id="mySidenav" class="sidenav">
-      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-      <a href="">Checking Account</a>
-      <a href="">Savings Account</a>
-      <a href="">Investing</a>
-      <a href="">Better Money Habits</a>
-    </div>
-
-    <div id="main">
-
-      <div class="header">
-        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; MENU</span>
-        <!TOP BAR>
-        <a href="homepage.php", class="logo", style="color: #FFFFE0">Bank Name</a>
-      </div>
-
-      <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <div class="header-right">
-          <a class="active" href="login.php">Sign In</a>
-          <a class="active" href="register.php">Register</a>
-        </div>
-      </div>
-      <br>
-
-      <!Register>
-      <div class = "section">
-        <div class = "row">
-          <div class="leftcolumn">
-            <img src="space.jpg" alt="Image 2">
-          </div>
-
-          <div class="rightcolumn">
-            <h1>Put Some Catchy Logo Here</h1>
-            <p>Put some other stuff here and add a button</p>
-            <br>
-            <a href = "register.php", button class="button button1">Register</a>
-          </div>
-        </div>
-      </div>
-
-      <br>
-
-      <!something else>
-      <div class = "section">
-        <div class="row">
-          <div class="leftcolumn">
-            <h1>Put Some Catchy Logo Here</h1>
-            <p>Put some other stuff here and add a button</p>
-            <br>
-            <a href = "", button class="button button1">This can go to something</a>
-          </div>
-
-          <div class="leftcolumn">
-            <img src="money.jpg" alt="Image 2" style="border-radius:50%">
-          </div>
-        </div>
-      </div>
-
-      <!Testemonials>
-      <br>
-      <div class = "section">
-        <br>
-        <div class="slideshow-container">
-          <div class="mySlides">
-            <q>I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
-            <p class="author">- John Keats</p>
-          </div>
-
-          <div class="mySlides">
-            <q>But man is not made for defeat. A man can be destroyed but not defeated.</q>
-            <p class="author">- Ernest Hemingway</p>
-          </div>
-
-          <div class="mySlides">
-            <q>I have not failed. I've just found 10,000 ways that won't work.</q>
-            <p class="author">- Thomas A. Edison</p>
-          </div>
-
-          <a class="prev" onclick="plusSlides(-1)">❮</a>
-          <a class="next" onclick="plusSlides(1)">❯</a>
-
-        </div>
-
-        <div class="dot-container">
-          <span class="dot" onclick="currentSlide(1)"></span>
-          <span class="dot" onclick="currentSlide(2)"></span>
-          <span class="dot" onclick="currentSlide(3)"></span>
-        </div>
-
-        <script>
-          var slideIndex = 1;
-          showSlides(slideIndex);
-
-          function plusSlides(n) {
-            showSlides(slideIndex += n);
-          }
-
-          function currentSlide(n) {
-            showSlides(slideIndex = n);
-          }
-
-          function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active";
-          }
-        </script>
-
-        <br>
-      </div>
-
-      <br>
-    </div>
-
-    <script>
-      function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
+<style>
+    * {
+    box-sizing: border-box;
+  }
+/*BODY TEXT*/
+  body {
+    font-family:Georgia;
+    color: #05386B;
+    padding: 5px;
+    background:	#a8e4a0;
+    margin: 0;
+  }
+/* HEADER
+      .header {
+        padding: 30px;
+        text-align: center;
+        border-style: solid;
+        border-width: thick;
+        border-color: lightyellow;
+        border-radius: 25px;
+        background-image:url("https://image.shutterstock.com/image-vector/natural-forest-trees-mountains-horizon-260nw-1588730179.jpg");
       }
-      function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft= "0";
-      }
-    </script>
+      .header h1 {
+        font-size: 50px;
+        font-weight: bold;
+        color: lightyellow;
+        font-family: monospace;
+      } */
 
-  </body>
-</html>
+/*ACCOUNT CARDS*/
+/* Left column */
+  .leftcolumn {
+    float: left;
+    margin-left: 20px;
+    padding: 30px;
+    width: 40%;
+    height: 30%;
+    padding-bottom:5%;
+  }
+
+/* Right column */
+  .rightcolumn {
+    float: right;
+    margin-right: 20px;
+    padding: 30px;
+    width: 40%;
+    height: 30%;
+  }
+
+  .account {
+    background-color: #EDF5E1;
+    padding: 20px;
+    margin-top: 20px;
+  }
+  .account a{
+    float: left;
+    display: block;
+    background-color: #5cdb95;
+    color: #EDF5E1;
+    text-align: center;
+    text-decoration: none;
+  }
+
+/* Clear floats after the columns */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+/*TOP LINK BAR*/
+  .topnav {
+    overflow: hidden;
+    background-color: lightyellow;
+  }
+  .topnav2 {
+    overflow: hidden;
+    background-color:  #5cdb95;
+  }
+  .topnav a{
+		float: left;
+		display: block;
+		color: #379683;
+		text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+	}
+  .topnav2 a{
+    float: left;
+    display: block;
+    color: #EDF5E1;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  /* Change color on hover */
+  .topnav a:hover {
+      background-color:  dodgerblue;
+      color: white;
+  }
+  .topnav2 a:hover {
+      background-color:  dodgerblue;
+      color: white;
+  }
+  .topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+  }
+
+/*BEGIN: SIDE MENU STUFF*/
+  .sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #8FBC8F;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+  }
+  .sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: lightyellow;
+    display: block;
+    transition: 0.3s;
+  }
+  .sidenav a:hover {
+    color: #818181;
+  }
+  .sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+  }
+  #main {
+    transition: margin-left .5s;
+    padding: 16px;
+  }
+  @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+  }
+/*END: SIDE MENU STUFF*/
+
+/*BEGIN: Header*/
+  .header {
+    overflow: hidden;
+    background-color: #f1f1f1;
+    border-width: thick;
+    border-top: none;
+    padding: 50px 20px;
+    background-image:url("https://image.shutterstock.com/image-vector/natural-forest-trees-mountains-horizon-260nw-1588730179.jpg");
+  }
+  .header a {
+    float: center;
+    color: black;
+    text-align: center;
+    padding: 12px;
+    text-decoration: none;
+    font-size: 18px;
+    line-height: 25px;
+    border-radius: 4px;
+
+  }
+  .header a.logo {
+    font-size: 50px;
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+    position:relative;
+    left: 30%;
+  }
+  .header a:hover {
+    background-color: #ddd;
+    color: black;
+  }
+  .header a.active {
+    background-color: dodgerblue;
+    color: white;
+    margin-top: 0px;
+  }
+  .header-right {
+    float: right;
+  }
+  @media screen and (max-width: 500px) {
+    .header a {
+      float: none;
+      display: block;
+      text-align: left;
+    }
+    .header-right {
+      float: none;
+    }
+  }
+/*END: Header*/
+
+/*BEGIN: SECTION*/
+  .section{
+    padding-bottom: 5px;
+    margin-left: 5%;
+    margin-right: 5%;
+    background: #FFFFE0;
+  }
+/*END: SECTION*/
+
+/*BEGIN: IMAGE*/
+  img{
+    height: 290px;
+    width: 330px;
+    margin-left: 100px;
+    opacity: .87;
+  }
+
+  img:hover {
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+  }
+/*END: IMAGE*/
+
+/*BEGIN: TEXT STUFF*/
+  h1 {
+    text-align: center;
+    letter-spacing: 3px;
+    font-size: 25px;
+    text-transform: uppercase;
+    color: #1e90ff;
+  }
+
+h2{
+      text-align: center;
+      letter-spacing: 3px;
+      font-size: 30px;
+      text-transform: uppercase;
+      color:  #5cdb95;
+}
+
+  p{
+    text-align: justify;
+    letter-spacing: 3px;
+    color: #004242;
+    text-decoration-line: black;
+  }
+/*END: TEXT STUFF*/
+
+/*BEGIN: Buttons*/
+  .button {
+    background-color: #fffafa;
+    border-radius: 0%;
+    color: #b0e0e6;
+    padding: 10px 25px;
+    text-align: center;
+    text-decoration:none;
+    display:block;
+    font-size: 25px;
+    margin: 2px 1px;
+    cursor: pointer;
+    font-family:times;
+  }
+  .button1 {
+    background-color: #4CAF50;
+    color: #fffafa;
+    border: 3px double #4CAF50;
+  }
+  .button1:hover {
+    background-color: #FFFFE0;
+    color: #1e90ff;
+  }
+/*END: Buttons*/
+
+/*BEGIN: SlideShow*/
+  /* Slideshow container */
+  .slideshow-container {
+    position: relative;
+    background: lightyellow;
+  }
+  /* Slides */
+  .mySlides {
+    display: none;
+    padding: 80px;
+    text-align: center;
+    overflow: auto;
+  }
+  /* Next & previous buttons */
+  .prev, .next {
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    width: auto;
+    margin-top: -30px;
+    padding: 16px;
+    color: #8FBC8F;
+    font-weight: bold;
+    font-size: 20px;
+    border-radius: 0 3px 3px 0;
+    user-select: none;
+  }
+  /* Position the "next button" to the right */
+  .next {
+    position: absolute;
+    right: 0;
+    border-radius: 3px 0 0 3px;
+  }
+  /* On hover, add a black background color with a little bit see-through */
+  .prev:hover, .next:hover {
+    background-color: #8FBC8F;
+    color: #fffafa;
+  }
+  /* The dot/bullet/indicator container */
+  .dot-container {
+      text-align: center;
+      padding: 20px;
+      background: #8FBC8F;
+  }
+  /* The dots/bullets/indicators */
+  .dot {
+    cursor: pointer;
+    height: 15px;
+    width: 15px;
+    margin: 0 2px;
+    background-color: #a8e4a0;
+    border-radius: 50%;
+    display: inline-block;
+    transition:0.5s ease;
+  }
+  /* Add a background color to the active dot/circle */
+  .active, .dot:hover {
+    background-color: #fffafa;
+  }
+  /* Add an italic font style to all quotes */
+  q {font-style: italic;}
+  /* Add a blue color to the author */
+  .author {
+    text-align: center;
+    color: cornflowerblue;
+  }
+/*END: SlideShow*/
+
+#form
+{ position: float;
+ background-color: #1111111;
+ height: 600px;
+ width: 700px;
+ margin-right: auto;
+ margin-left:  auto;
+ border-color: #39ac73;
+ border-style: outset;
+
+}
+.form-group
+{
+  position: float;
+}
+
+label
+{
+  font-family: Ariel;
+  font-size: 10px;
+  color: white;
+  font-size: 30px;
+  color: #333;
+  height: 20px;
+  width: 150px;
+  margin-top: 50px;
+  margin-left: 100px;
+  text-align: justify;
+  margin-right: 0px;
+  float: left;
+}
+
+input
+{
+  height: 20px;
+  width: 300px;
+  border: 1px solid #000;
+  margin-top: 50px;
+  margin-left: 50px;
+  margin-right: 40px;
+}
+
+#submit
+{
+  font-family: Ariel;
+  margin-left: 350px;
+  width: 200px;
+  height: 40px;
+  font-size: 20px;
+  background-color: lightgreen;
+  border-color: #39ac73;
+  border-style: outset;
+}
+
+</style>
--- CMPE131PROJECT-master/login.php
+++ CMPE131PROJECT-master/login.php
@@ -17,7 +17,7 @@ if (isset($_POST["username"]) && isset($_POST["password"])){
             $sql = "SELECT * FROM useraccounts WHERE userID=? OR email=?;";
             $stmt = mysqli_stmt_init($conn);
             if(!mysqli_stmt_prepare($stmt, $sql)) {
-                  echo "SQL error";
+                  echo "first if error";
                   exit();
             }
             else{
@@ -29,27 +29,26 @@ if (isset($_POST["username"]) && isset($_POST["password"])){
                         $pwdCheck = password_verify($pin, $row['pin']);
                         echo ( 'Pin: '.$row['pin'] );
                         if(!($pin == $row['pin'])){
-                              echo "Error: password incorrect.";
+                              echo "password check error";
                         }
                         else{
                               session_start();
                               $_SESSION['userID']= $row['userID'];
-
-                              echo("Session ID: " .$_SESSION['userID']);
                               $_SESSION['fname']= $row['fname'];
                               $_SESSION['lname']= $row['lname'];
                               $_SESSION['username']= $row['username'];
                               $_SESSION['email']= $row['email'];
-
+                              header('Location: /CMPE131PROJECT-master/test_userpage.php');
                         }
                   }
                   else{
-                        echo "Data match error.";
+                        echo "error found";
                   }
             }
       }
 }
 else {
+      echo "A field is empty."; //will remove this once I figure some stuff out
 }
 ?>
 
@@ -57,55 +56,56 @@ else {
   <head>
     <meta charset="utf-8">
     <title>Login</title>
+
     <link rel="stylesheet" href="homepage.css">
-      </head>
+	<!At this point in time, I will be using the registerStyle for the login page as well>
+</head>
 
-    <body><!This is the login page>
-     <!MENU BAR>
-     <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="">Checking Account</a>
-        <a href="">Savings Account</a>
-        <a href="">Investing</a>
-        <a href="">Better Money Habits</a>
-     </div>
+<body><!This is the login page>
+<!MENU BAR>
+<div id="mySidenav" class="sidenav">
+ <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+ <a href="">Checking Account</a>
+ <a href="">Savings Account</a>
+ <a href="">Investing</a>
+ <a href="">Better Money Habits</a>
+</div>
 
-     <div id="main">
+<div id="main">
 
-       <div class="header">
-         <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; MENU</span>
-         <!TOP BAR>
-         <a href="homepage.php", class="logo", style="color: #FFFFE0">Bank Name</a>
-      </div>
+ <div class="header">
+   <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; MENU</span>
+   <!TOP BAR>
+   <a href="homepage.php", class="logo", style="color: #FFFFE0">Bank Name</a>
+</div>
 
-      <div class="topnav">
-        <a class="active" href="homepage.php">Home</a>
-        <a href="homepage.php#news">News</a>
-        <a href="homepage.php#contact">Contact</a>
-        <a href="homepage.php#about">About</a>
-        <div class="header-right">
+<div class="topnav">
+       <a class="active" href="homepage.php">Home</a>
+       <a href="homepage.php#news">News</a>
+       <a href="homepage.php#contact">Contact</a>
+       <a href="homepage.php#about">About</a>
+       <div class="header-right">
           <a class="active" href="register.php">Register</a>
-        </div>
-      </div>
-      <br>
+       </div>
+</div>
+<br>
 
-
-  <div id= "form">
-        <h2>Login</h2>
-          <form action = "login.php" method="post">
-                  <input type= "text" name = "username" placeholder = "username/email">
-                  <input type= "password" name = "password" placeholder = "pin">
-            <button type = "submit" name= "login-submit">Login</button>
-      	</form>
-            <main>
-                  <?php
-                        if(isset($_SESSION['username'])){
-                              echo '<p>Logged IN</p>';
-                        }
-                        else{
-                              echo '<p>Logged OUT</p>';
-                        }
-                  ?>
-            </main>
+<div id= "form">
+ <h2>Login</h2>
+    <form action = "login.php" method="post">
+            <input type= "text" name = "username" placeholder = "username/email">
+            <input type= "password" name = "password" placeholder = "pin">
+      <button type = "submit" name= "login-submit">Login</button>
+      </form>
+      <main>
+            <?php
+                  if(isset($_SESSION['username'])){
+                        echo '<p>Logged IN</p>';
+                  }
+                  else{
+                        echo '<p>Logged OUT</p>';
+                  }
+            ?>
+      </main>
 </div>
 </body>
