b34c81006f46e56744feaf2eafc2400987f298ce
aman-kaur11 <63080661+aman-kaur11@users.noreply.github.com>
2020-05-06 03:31:14 -0700

    Update deleteAccount.php

--- deleteAccount.php
+++ deleteAccount.php
@@ -1,29 +1,59 @@
 <html>
-  <table border = 1 cellpadding = 1 cellspacing =1>
-    <tr>
-      <th>User ID</th>
-      <th>Account Number</th>
-      <th>Account Name</th>
-      <th>Balance</th>
-    </tr>
+
+<head> <!This is the title of the webpage>
+  <meta charset="utf-8">
+  <title>Delete Account</title>
+  <link rel="stylesheet" href="deposit.css">
+</head>
+
+<div class = "row">
+     <div id = "grad1", class = "header">
+       <h1><p class = "custom1"> BANK NAME</p></h1>
+    </div>
+     <div id = "">
+       <div class="topnav">
+         <a href="logout.php" style="float: right;"> Sign Out</a>
+         <a href="homepage.php" style="float: left;"> Return</a>
+        </div>
+</div>
+
+
+
+<?php
+  session_start();
+  //connnect to localhost
+  $conn = mysqli_connect("localhost","root","","userbank");
+  //check connection
+  if(!$conn){
+      die("Connection failed" . mysqli_connect_error());
+  }
+?>
+
+  <div class="leftcolumn">
+    <div class="account">
+    <h2>Delete From Following:</h2>
     <?php
-      session_start();
-      //connnect to localhost
-      $conn = mysqli_connect("localhost","root","","userbank");
-      echo "connected! <br>";
-      //check connection
-      if(!$conn){
-          die("Connection failed" . mysqli_connect_error());
-      }
-      if (isset($_POST["userID"]))
-      {
+      if(isset($_SESSION['userID'])) {
+
       $userid = $_SESSION['userID'];
+
       //select $query
-      echo "Accounts whose balance is zero are only displayed<br>";
       $sql = "SELECT * FROM accounts WHERE userID = '$userid' AND balance = 0";
+      $sql1 = "SELECT COUNT(*) FROM accounts where userID = '$userid' AND balance = 0";
+      $rs = mysqli_query($conn,$sql1);
+      $result1 = mysqli_fetch_array($rs);
 
       //execute $query
+      if($result1[0] >= 1){
       $records = mysqli_query($conn,$sql);
+
+      echo "<table border = 1 cellpadding = 1 cellspacing =1>
+          <tr>
+            <th>User ID</th>
+            <th>Account Number</th>
+            <th>Account Name</th>
+            <th>Balance</th>
+          </tr>";
       while($row = mysqli_fetch_array($records))
       {
         echo "<tr>";
@@ -34,16 +64,73 @@
         echo "<td><a href = delete.php?id=".$row['acctNum'].">Delete</a></td>";
 
       }
+      echo "</table>";
+
       $results = mysqli_query($conn, $sql);
       if ($results) {
-          echo "."; //As a toast message
+          echo ""; //As a toast message
           //we can output the account info after
       } else {
           echo mysqli_error($conn);
           echo "something";
         }
-      }
-    ?>
-  </table>
 
+    }
+  }  elseif ($result1[0] <= 1) {
+    echo "User must have at least 1 active account";
+  }
+  ?>
+  </div>
+  </div>
+
+  <div class="rightcolumn">
+    <div class="account">
+      <h2>Transfer all funds before deleting<br></h2>
+
+      <?php
+      if(isset($_SESSION['userID'])) {
+
+      $userid = $_SESSION['userID'];
+
+      //select $query
+      $sql = "SELECT * FROM accounts WHERE userID = '$userid' AND balance != 0";
+      $sql1 = "SELECT COUNT(*) FROM accounts where userID = '$userid' AND balance != 0";
+      $rs = mysqli_query($conn,$sql1);
+      $result1 = mysqli_fetch_array($rs);
+
+      //execute $query
+      if($result1[0] >= 1){
+      $records = mysqli_query($conn,$sql);
+      echo "<table border = 1 cellpadding = 1 cellspacing =1>
+          <tr>
+            <th>User ID</th>
+            <th>Account Number</th>
+            <th>Account Name</th>
+            <th>Balance</th>
+          </tr>";
+      while($row = mysqli_fetch_array($records))
+      {
+        echo "<tr>";
+        echo "<td>".$row['userID']."</td>";
+        echo "<td>".$row['acctNum']."</td>";
+        echo "<td>".$row['acctName']."</td>";
+        echo "<td>".$row['balance']."</td>";
+        echo "<td><a href = transfer.php?id=".$row['acctNum'].">Transfer</a></td>";
+
+      }
+      echo "</table>";
+
+      $results = mysqli_query($conn, $sql);
+      if ($results) {
+
+      } else {
+          echo mysqli_error($conn);
+      }
+    }
+  } else if ($result1[0] <= 1) {
+    echo "User must have at least 1 active account";
+  }
+  ?>
+    </div>
+  </div>
 </html>
