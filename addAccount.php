5782bd7794b259f00a808db28a75434f076f4ecb
laurenmacpherson <60455659+laurenmacpherson@users.noreply.github.com>
2020-05-07 11:11:54 -0700

    Add files via upload
    Added a validation so bank accounts can't have the same name

--- addAccount.php
+++ addAccount.php
@@ -10,27 +10,46 @@
   $query = "SELECT * FROM accounts";
   $result1 = mysqli_query($conn, $query);
 
-
+  $message = "";
   /*BEGIN: ADD AN ACCOUNT*/
   if (isset($_POST["acctName"]))
   {
     if ($_POST["acctName"])
     {
+      $valid = TRUE;
       $userid = $_SESSION['userID'];
       $acctname= $_POST['acctName'];
       $balance = $_POST['balance'];
-     
       //expand random number range if needed
       //Register user
-      $sql = "INSERT INTO accounts (userID, acctName, balance) VALUES
-      ('$userid','$acctname','$balance')";//starting balance in each account it zero
-      // echo $sql;
-      $results = mysqli_query($conn, $sql);
-      if ($results) {
-        header("Location: /customAccounts.php"); 
-      } else {
-        echo mysqli_error($conn);
-        echo "something 1";
+
+      $validation = "SELECT acctName FROM accounts WHERE userID='$userid' AND acctName = '$acctname' ";
+
+      $valresult = $conn->query($validation);
+
+      if ($valresult->num_rows > 0) {
+        while($valrow = $valresult->fetch_assoc()) {
+             if ($valrow["acctName"] == $acctname){      //if there's already an acct with that name
+                   $valid = FALSE;
+             }
+        }
+      }
+
+      if ($valid){
+            $sql = "INSERT INTO accounts (userID, acctName, balance) VALUES
+            ('$userid','$acctname','$balance')";//starting balance in each account it zero
+            // echo $sql;
+            $results = mysqli_query($conn, $sql);
+            if ($results) {
+              $message =  "New account added: $acctname [Balance: $$balance]"; //As a toast message
+              //we can output the account info after
+            } else {
+              echo mysqli_error($conn);
+              echo "something 1";
+            }
+      }
+      else {
+            $message =  "Error: You already have a bank account with that name. Please choose a new name.";
       }
     } else {
       echo "A field is empty."; //Also as a toast message
@@ -64,15 +83,18 @@
       <h1> Register Account</h1>
       <form action = "addAccount.php" method="post">
         <div class = "form-group">
-          <label for = "acctName"> account name: </label>
+          <label for = "acctName"> Account Name: </label>
           <input name = "acctName" type = "text" required>
           <p></p>
-          <label for = "balance"> initial balance: </label>
+          <label for = "balance"> Initial Balance ($): </label>
           <input name = "balance" type = "number" required>
           <p></p>
         </div>
-        <input type = "submit" name = "submit" id = "submit">
+        <button type = "submit" name = "submit" id = "submit">Add Account</button>
       </form>
+      <?php
+            echo $message;
+       ?>
     </div>
   </div>
 </body>
